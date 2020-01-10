5f4fd5000457a901739796a129d2adecc3c1160b
RolandMQuiros <afictitiousforce@gmail.com>
2020-01-09 23:48:19 -0800

    Updated to 2019.3.0f3

--- Assets/Plugins/Redux/Async/Thunk.cs
+++ Assets/Plugins/Redux/Async/Thunk.cs
@@ -1,13 +1,8 @@
 using System;
-using System.Threading.Tasks;
-
-using UniRx.Async;
 
 namespace Redux
 {
     public delegate void ActionsCreator<TState>(Dispatcher dispatcher, Func<TState> getState);
-    public delegate Task AsyncActionsCreator<TState>(Dispatcher dispatcher, Func<TState> getState);
-    public delegate UniTask UnityAsyncActionsCreator<TState>(Dispatcher dispatcher, Func<TState> getState);
 
     public static partial class Middlewares
     {
@@ -16,16 +11,6 @@ namespace Redux
                 var actionsCreator = action as ActionsCreator<TState>;
                 if (actionsCreator != null) {
                     actionsCreator(dispatch, store.GetState);
-                }
-
-                var asyncActionsCreator = action as AsyncActionsCreator<TState>;
-                if (asyncActionsCreator != null) {
-                    asyncActionsCreator(dispatch, store.GetState);
-                }
-
-                var uniTaskActionsCreator = action as UnityAsyncActionsCreator<TState>;
-                if (uniTaskActionsCreator != null) {
-                    uniTaskActionsCreator(dispatch, store.GetState);
                 }
 
                 return action;
